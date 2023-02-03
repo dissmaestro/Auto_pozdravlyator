65b6c3f4d0af14563aa133e7c34c62d654d05c15
dissmaestro <masimka1302@yandex.ru>
2023-02-03 20:24:13 +0500

    create db sqlite3 and try to insert date

--- Birthday_congrats/main.py
+++ Birthday_congrats/main.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3 as sq
 import vk_api
 
 #  В этом блоке кода представлены функции реализующие получение информации от вк
@@ -6,7 +6,7 @@ import vk_api
 vk_session = vk_api.VkApi('89257828987', 'Maks1302')  # 89257828987  Maks1302
 vk_session.auth()
 vk = vk_session.get_api()
-my_id = 81028675
+my_id = 781028675 #781028675
 
 
 def get_friends_id(user_id: int):
@@ -41,24 +41,49 @@ def get_friends_lname(id_friend: list): # return list of last name
         Lname.append(lname_clear)
     return Lname
 
+def list_of_tuple(list_id: list, bdate: list, name: list, lname: list):
+    list_of_all = list()   # преобразование кортежей в список кортежей
+
+    for i in range(len(list_id)):
+        list_of_one = list()
+        list_of_one.append(list_id[i])
+        list_of_one.append(name[i])
+        list_of_one.append(lname[i])
+        list_of_one.append(bdate[i])
+        list_of_all.append(tuple(list_of_one))
+    return list_of_all
 
 list_of_id = get_friends_id(my_id)
-print(get_friends_bdate(list_of_id))
-print(get_friends_name(list_of_id))
-print(get_friends_lname(list_of_id))
 
-#  В этом блоке кода представлена реализация бд на  Sqlite3  а так де запись в бд данныз из вк
+list_of_bdate = get_friends_bdate(list_of_id)
+list_of_name = get_friends_name(list_of_id)
+list_of_lname = get_friends_lname(list_of_id)
+
+list_tuple = list_of_tuple(list_of_id, list_of_bdate, list_of_name, list_of_lname)
+            #  В этом блоке кода представлена реализация бд на  Sqlite3  а так де запись в бд данныз из вк
 # ----------------------------------------------------------------------------
 
-conn = sqlite3.connect(r'./congrats.db') # cozdaine DB
-cur = conn.cursor() # объект для реализации запросов
 
-# crate a table
+try:
+    conn = sq.connect(r'./congrats.db')  # cozdaine DB
+    cur = conn.cursor()  # объект для реализации запросов
+    print("Подключен к SQLite")
 
-#cur.execute('''CREATE TABLE IF NOT EXISTS users(
-#    user_id INT PRIMARY KEY,
-#    fname TEXT
-#    lname TEXT
-#    bdate TEXT);
- #   ''')
-#conn.commit()
+    cur.execute('''CREATE TABLE IF NOT EXISTS MyFriends(
+        user_id INT PRIMARY KEY,
+        fname TEXT
+        lname TEXT
+        bdate TEXT);
+        ''')
+
+    cur.executemany(''''INSERT INTO MyFriends VALUES(?, ?, ?, ?)''', list_tuple)
+    conn.commit()
+    print("Запись успешно вставлена ​​в таблицу sqlitedb_developers ", cur.rowcount)
+    cur.close()
+
+except sq.Error as error:
+    print("Ошибка при работе с SQLite", error)
+finally:
+    if conn:
+        conn.close()
+        print("Соединение с SQLite закрыто")
