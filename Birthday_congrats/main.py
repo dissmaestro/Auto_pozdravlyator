6bdeacb4dfd7529fc94703e66eda02cbe51504d7
dissmaestro <masimka1302@yandex.ru>
2023-02-03 18:04:51 +0500

    zakoncheno s func vk

--- Birthday_congrats/main.py
+++ Birthday_congrats/main.py
@@ -1,13 +1,12 @@
-import vk
-import requests
+import sqlite3
 import vk_api
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-vk_session = vk_api.VkApi('89257828987', 'Maks1302')
+#  В этом блоке кода представлены функции реализующие получение информации от вк
+# ----------------------------------------------------------------------------
+vk_session = vk_api.VkApi('89257828987', 'Maks1302')  # 89257828987  Maks1302
 vk_session.auth()
 vk = vk_session.get_api()
-my_id = 781028675
+my_id = 81028675
 
 
 def get_friends_id(user_id: int):
@@ -15,44 +14,51 @@ def get_friends_id(user_id: int):
     return id_fr.get('items')
 
 
-def get_friends_bdate(id_friend: list):
+def get_friends_bdate(id_friend: list):  # return list of id frirnd
     birthday = list()
     for i in range(len(id_friend)):
-        bdat_json = (vk_session.method("friends.get", {"fields": {"bdate": int(id_friend[i])}}))
-        bdate_clear = bdat_json.get('items')[i].get('bdate')
+        bdate_json = (vk_session.method("friends.get", {"fields": {"bdate": int(id_friend[i])}}))
+        bdate_clear = bdate_json.get('items')[i].get('bdate')
         birthday.append(bdate_clear)
 
     return birthday
 
 
+def get_friends_name(id_friend: list):   # return list of first name
+    Name = list()
+    for i in range(len(id_friend)):
+        Name_json = (vk_session.method("friends.get",{"fields": {"first_name": int(id_friend[i])}}))
+        Name_clear = Name_json.get('items')[i].get('first_name')
+        Name.append(Name_clear)
+    return Name
 
-h = get_friends_id(my_id)
-print(get_friends_bdate(h))
 
-# print(vk.wall.post(message = 'Kuki muki'))
+def get_friends_lname(id_friend: list): # return list of last name
+    Lname = list()
+    for i in range(len(id_friend)):
+        lname_json = (vk_session.method("friends.get", {"fields": {"last_name": int(id_friend[i])}}))
+        lname_clear = lname_json.get('items')[i].get('last_name')
+        Lname.append(lname_clear)
+    return Lname
 
-'''
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
-------------------------------------------------------
-my_id = 781028675  # moi id v vk
-token = "0b4f6e210b4f6e210b4f6e2131085d00af00b4f0b4f6e21689672b7ead64a1ebf3b706f"  # Сервисный ключ доступа
-my_id, login, password = '781028675', '89257828987', '13022002m'
-session = vk.AuthSession(my_id, login, password,)
-vk_api = vk.API(session)
-session = vk.session(access_token='0b4f6e210b4f6e210b4f6e2131085d00af00b4f0b4f6e21689672b7ead64a1ebf3b706f')
-api = vk.API(session)
 
-param = {'access_token': token, 'user_id': my_id, 'v': 5.131}
-method = 'friends.get'
-rec = requests.get(url=f'https://api.vk.com/method{method}', params=param)
-print(rec.json())
-def get_friends_id(user_id):
-    friends = session.method("friends.get", {"user_id": user_id})
-    '''
+list_of_id = get_friends_id(my_id)
+print(get_friends_bdate(list_of_id))
+print(get_friends_name(list_of_id))
+print(get_friends_lname(list_of_id))
+
+#  В этом блоке кода представлена реализация бд на  Sqlite3  а так де запись в бд данныз из вк
+# ----------------------------------------------------------------------------
+
+conn = sqlite3.connect(r'./congrats.db') # cozdaine DB
+cur = conn.cursor() # объект для реализации запросов
+
+# crate a table
+
+#cur.execute('''CREATE TABLE IF NOT EXISTS users(
+#    user_id INT PRIMARY KEY,
+#    fname TEXT
+#    lname TEXT
+#    bdate TEXT);
+ #   ''')
+#conn.commit()
